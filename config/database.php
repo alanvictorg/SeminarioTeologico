02856b182c57cd98dc598fb0e9cf63e13bf3c5cd
alanvictorg <alanvictorgp@gmail.com>
2017-06-15 19:03:11 -0300

    Convert to use Heroku PostgreSQL database

--- config/database.php
+++ config/database.php
@@ -1,5 +1,12 @@
 <?php
 
+$url = parse_url(getenv("DATABASE_URL"));
+
+$host = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$database = substr($url["path"], 1);
+
 return [
 
     /*
@@ -13,7 +20,9 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => 'pgsql',
+
+//env('DB_CONNECTION', 'mysql')
 
     /*
     |--------------------------------------------------------------------------
@@ -30,6 +39,7 @@ return [
     | choice installed on your machine before you begin development.
     |
     */
+
 
     'connections' => [
 
@@ -55,11 +65,11 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => $host, //env('DB_HOST', '127.0.0.1'),
+           // 'port' => env('DB_PORT', '5432'),
+            'database' => $database, //env('DB_DATABASE', 'forge'),
+            'username' => $username, //env('DB_USERNAME', 'forge'),
+            'password' => $password, //env('DB_PASSWORD', ''),
             'charset' => 'utf8',
             'prefix' => '',
             'schema' => 'public',
